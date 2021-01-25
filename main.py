3fed0e333030fa5dabd5e90aa7e2728628a531ae
heimdall <hemidal@gmail.com>
2021-01-25 22:10:41 +0100

    Adding page menu and page support;

--- main.py
+++ main.py
@@ -4,21 +4,37 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 from analysis.mpi import MoneyFlowIndex
+from pages.portfolio import PortfolioOptimization
+from pages.technical import TechnicalAnalysis
 
+# App configuration
 st.set_page_config(page_title="Finance App")
 plt.style.use('seaborn')
 
+# App sidebar
 st.sidebar.header('Pages')
-sides = st.sidebar.radio("", ('Technical analysis', 'Portfolio Optimization'))
+
+
+def show_pages_menu():
+    return st.sidebar.radio("", ('Technical analysis', 'Portfolio Optimization'))
+
+
+def show_page():
+    if pages == 'Technical analysis':
+        technical_analysis = TechnicalAnalysis(st)
+        technical_analysis.show()
+    elif pages == 'Portfolio Optimization':
+        portfolio = PortfolioOptimization(st)
+        portfolio.show()
+
+
+pages = show_pages_menu()
+show_page()
 
 st.sidebar.header('User panel')
 
 input_type = st.sidebar.radio("Select the data type", ('FILE', 'YAHOO API'))
 df = pd.DataFrame()
-
-st.write("""
-# Technical analysis
-""")
 
 if input_type == 'YAHOO API':
     ticker_symbol = st.sidebar.text_input('Enter Ticker Symbol')
---
+++ pages/portfolio.py
@@ -0,0 +1,8 @@
+class PortfolioOptimization:
+    def __init__(self, st):
+        self.st = st
+
+    def show(self):
+        self.st.write("""
+        # Portfolio Optimization
+        """)
